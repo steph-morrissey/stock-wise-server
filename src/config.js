74231626efd9749128fda1a99e4d969bc9c9890c
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-09-12 14:02:43 +0100

    Fixed lint

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const PORT = process.env.PORT || 8000;
 
-export const DB_URI =
-  process.env.MONGODB_URI || 'mongodb://localhost/inventory';
+export const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/inventory';
 
 export const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
