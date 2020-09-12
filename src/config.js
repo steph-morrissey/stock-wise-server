84fbcbe3a04a884ead0a089260537dd6079d7a15
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-09-12 14:00:37 +0100

    Altered exports from default

--- src/config.js
+++ src/config.js
@@ -1,15 +1,10 @@
-const PORT = process.env.PORT || 8000;
+export const PORT = process.env.PORT || 8000;
 
-const DB_URI = process.env.MONGODB_URI || 'mongodb://localhost/inventory';
+export const DB_URI =
+  process.env.MONGODB_URI || 'mongodb://localhost/inventory';
 
-const MONGOOSE_OPTIONS = {
+export const MONGOOSE_OPTIONS = {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true,
-};
-
-export default {
-  PORT,
-  DB_URI,
-  MONGOOSE_OPTIONS,
 };
