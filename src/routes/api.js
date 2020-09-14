94677a2609b3c9f39b6213b9de66fd187c3fd323
Stephanie Morrissey <sgmorrissey098@gmail.com>
2020-09-14 17:46:50 +0100

    Fixed lint

--- src/routes/api.js
+++ src/routes/api.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 const addCategory = async (req, res) => {
   try {
-    console.log("HERE", req.user);
     const payload = req.body;
 
     db.Category.create(payload);
